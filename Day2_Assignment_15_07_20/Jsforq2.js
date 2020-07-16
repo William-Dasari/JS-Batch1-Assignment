f960635da0e10455119ae742ea028f796111e089
William-Dasari <66524338+William-Dasari@users.noreply.github.com>
2020-07-16 13:59:16 +0530

    Add files via upload

---
+++ Day2_Assignment_15_07_20/Jsforq2.js
@@ -0,0 +1,52 @@
+//Example of CharCodeAt//
+let str= "Assignment";
+console.log(str.charCodeAt(0));
+
+console.log(String.fromCharCode(65));
+
+let str1="Example for include() in string"
+console.log(str1.includes("string"));
+
+let str2="king"
+let str3="Queen"
+console.log(str1.localeCompare(str2))
+
+let str4="                I did it..!"
+console.log(str4.repeat(3))
+
+let str5="Day2 Assignment Completed"
+console.log(str5.replace("Assignment","task"));
+
+console.log(str5.search("Day2"))
+
+console.log(str5.slice(0,10));
+
+console.log(str4.startsWith("I"));
+
+console.log(str4.split(""));
+
+console.log(str4.substr(0,6));
+
+console.log(str4.substring(0,6));
+
+console.log(str4.trim());
+
+var cars = ["Tata", "Volvo", "BMW","Benz","toyota","suzuki"];
+
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
---
+++ Day2_Assignment_15_07_20/two.js
@@ -0,0 +1,4 @@
+//prompt("Enter your age ?")
+var value=prompt("Enter your name")
+console.log(value);
+document.write("The value entered by you is "+value);
